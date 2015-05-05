e51d9700f9c2797f368a2c90a88338eabbd23266
lucas <lucas@lucas-VirtualBox>
2015-05-05 18:33:59 -0300

    Adicionando constantes para referência a propriedades

---
+++ twm/twm-service/src/main/java/br/com/lptrias/twm/service/conf/GraphDataProperties.java
@@ -0,0 +1,13 @@
+package br.com.lptrias.twm.service.conf;
+
+
+/**
+ * 
+ * @author ltrias
+ *
+ * Contains constants to reference properties of vertexes, edges e indices on graph 
+ */
+public final class GraphDataProperties {
+	public static final String NAME = "name";
+	public static final String DISTANCE = "d";
+}
