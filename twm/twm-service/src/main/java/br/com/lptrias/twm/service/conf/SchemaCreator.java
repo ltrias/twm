665a9b1365461033405452fcc868c252074ec75f
lucas <lucas@lucas-VirtualBox>
2015-05-05 18:31:50 -0300

    Corrigindo pacote

--- twm/twm-service/src/main/java/br/com/lptrias/twm/service/conf/SchemaCreator.java
+++ twm/twm-service/src/main/java/br/com/lptrias/twm/service/conf/SchemaCreator.java
@@ -1,15 +1,15 @@
-package br.com.lptrias.twm.service.impl;
+package br.com.lptrias.twm.service.conf;
+
+import static br.com.lptrias.twm.service.conf.GraphDataProperties.*;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import com.thinkaurelius.titan.core.PropertyKey;
-import com.thinkaurelius.titan.core.TitanFactory;
 import com.thinkaurelius.titan.core.TitanGraph;
 import com.thinkaurelius.titan.core.schema.ConsistencyModifier;
 import com.thinkaurelius.titan.core.schema.TitanGraphIndex;
@@ -28,38 +28,32 @@ public class SchemaCreator implements InitializingBean{
 	
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		
 		LOGGER.debug("Creating DB schema");
 		
-//		Graph g = TitanFactory.open(ClassLoader.getSystemClassLoader().getResource("titan-cassandra-es.properties").getPath());
-//		TitanGraph tg = (TitanGraph) g;
-//		TitanManagement m = tg.getManagementSystem();
+		TitanGraph tg = (TitanGraph) graph;
+		TitanManagement m = tg.getManagementSystem();
 		
+		PropertyKey name = null;
+		if( !m.containsPropertyKey(NAME) ){
+			name = m.makePropertyKey(NAME).dataType(String.class).make();
+			TitanGraphIndex namei = m.buildIndex(NAME,Vertex.class).addKey(name).unique().buildCompositeIndex();
+			m.setConsistency(namei, ConsistencyModifier.LOCK);
+		}
 		
-		//LOGGER.debug("Graph: " + tg);
-//
-//		PropertyKey name = null;
-//		if( !m.containsPropertyKey("name") ){
-//			name = m.makePropertyKey("name").dataType(String.class).make();
-//			TitanGraphIndex namei = m.buildIndex("name",Vertex.class).addKey(name).unique().buildCompositeIndex();
-//			m.setConsistency(namei, ConsistencyModifier.LOCK);
-//		}
-//		
 //		if( !m.containsVertexLabel(LOCATION) ){
 //			m.makeVertexLabel(LOCATION).make();
 //		}
-//		
-//		if( !m.containsPropertyKey("d")) {
-//			PropertyKey distance = m.makePropertyKey("d").dataType(Integer.class).make();
-//			TitanGraphIndex distancei = m.buildIndex("d", Edge.class).addKey(distance).buildMixedIndex("search");
-//		}
-//		
+		
+		if( !m.containsPropertyKey(DISTANCE)) {
+			PropertyKey distance = m.makePropertyKey(DISTANCE).dataType(Integer.class).make();
+			TitanGraphIndex distancei = m.buildIndex(DISTANCE, Edge.class).addKey(distance).buildMixedIndex("search");
+		}
+		
 //		if( !m.containsEdgeLabel("acessa") ){
 //			m.makeEdgeLabel("acessa").directed().signature(m.getPropertyKey("d")).make();
 //		}
-//		
-//		m.commit();
 		
+		m.commit();	
 	}
 	
 }
